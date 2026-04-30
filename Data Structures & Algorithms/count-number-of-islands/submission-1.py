c984293f875378ee3a95dbd443174c42b0a622f9
Amir Ehsandar <ehsandaramir@gmail.com>
2026-04-30 14:58:04 +0330

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        cnt = 0
+        for i, row in enumerate(grid):
+            for j, cel in enumerate(grid[i]):
+                if cel == "1":
+                    cnt += 1
+                    self.propagate_from(grid, i, j)
+
+        return cnt
+
+    def propagate_from(self, grid, i, j):
+        if grid[i][j] == "1":
+            grid[i][j] = "x"
+
+        if 0 <= i+1 < len(grid) and grid[i+1][j] == "1":
+            self.propagate_from(grid, i+1, j)
+        if 0 <= i-1 < len(grid) and grid[i-1][j] == "1":
+            self.propagate_from(grid, i-1, j)
+        if 0 <= j+1 < len(grid[i]) and grid[i][j+1] == "1":
+            self.propagate_from(grid, i, j+1)
+        if 0 <= j-1 < len(grid[i]) and grid[i][j-1] == "1":
+            self.propagate_from(grid, i, j-1)
