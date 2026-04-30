abf784c551172eb21033939ad9af28204a480b10
Amir Ehsandar <ehsandaramir@gmail.com>
2026-04-30 15:30:13 +0330

    Add base.py to project

---
+++ base.py
@@ -0,0 +1,11 @@
+from collections import *
+from itertools import *
+from typing import List, Dict, Set, Optional, Protocol
+from copy import deepcopy
+import traceback
+import heapq
+import collections
+import sys
+import re
+import math
+import ast
